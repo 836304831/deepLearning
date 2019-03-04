53981a8bdc56c69b5600ac6f0760fd81f645d06f
unknown <836304831>
2019-03-04 08:24:18 +0800

    add vgg_tensorboard

---
+++ python_net/common/utils.py
@@ -0,0 +1,12 @@
+
+
+import os
+
+
+def path_generate(root, subpath):
+    path = os.path.join(root, subpath)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    if os.path.exists(path):
+        return True
+    return False
